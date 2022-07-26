26d43beb8d19572b60278253436d701ce365a1f7
João Marcelo Dantas <jmgrd98@gmail.com>
2022-07-26 07:35:22 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function contar(){
+    let inicio = document.getElementById('inicio').value;
+    let fim = document.getElementById('fim').value;
+    let passo = document.getElementById('passo').value;
+    let resultado = document.getElementById('resultado');
+
+    inicio = Number.parseInt(inicio);
+    fim = Number.parseInt(fim);
+    passo = Number.parseInt(passo);
+
+    if(inicio == 0 || fim == 0 || passo == 0){
+        alert('[ERRO] Insira os valores para fazer a contagem!');
+    }
+
+    else{
+        resultado.innerHTML = "Contado: "
+        
+    for(let contador = 0; contador <= fim; contador+=passo){
+            resultado.innerHTML += `${contador} 😀`
+    }
+
+}
+}
